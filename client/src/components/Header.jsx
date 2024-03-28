28e5024c5f58a3f99bf6d325798dcee212ade87b
DIGONTA HALDER <digantahalder14000@gmail.com>
2024-03-28 20:56:38 +0530

    commit: header & sidebar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,23 +6,12 @@ const Header = () => {
     <header className="bg-white shadow-lg">
       <nav className="container mx-auto flex justify-between items-center py-4 px-4 md:px-8">
         <div className="flex justify-end items-center w-full">
-          <div className="flex items-center mr-4">
-            <div className="flex items-center mr-4">
-              <input
-                type="text"
-                placeholder="Search..."
-                className="border border-gray-300 px-2 py-1 ml-2 focus:outline-none"
-              />
-            </div>
-
-            <IoIosNotifications className="text-gray-600" />
-          </div>
-
+          <IoIosNotifications className="text-gray-600" />
           <Link
             to="/login"
-            className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-sm"
+            className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-sm ml-4"
           >
-            Login / SignUp
+            Register
           </Link>
         </div>
       </nav>
