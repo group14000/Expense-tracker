f36250a24d4c1a43637c39dc6a5ddc83e4fdb2e4
DIGONTA HALDER <digantahalder14000@gmail.com>
2024-03-28 17:22:52 +0530

    commit: user - 5

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,8 +2,19 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      <Link to={"/sign-up"}>Sign Up</Link>
+    <div className="flex justify-between items-center p-4 bg-purple-700 text-white">
+      <div className="flex items-center">
+        <Link to={"*"} className="text-2xl font-bold">
+          BudgetBoss
+        </Link>
+      </div>
+      <div className="flex">
+        <Link to="/login" className="mr-4">
+          <div className="bg-yellow-400 rounded-full px-4 py-2 hover:bg-yellow-500 transition duration-300 ease-in-out">
+            <span className="font-semibold">Login / SignUp</span>
+          </div>
+        </Link>
+      </div>
     </div>
   );
 };
